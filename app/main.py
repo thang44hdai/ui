89deb4fbe54a7fd05734aec071546e2f008ee98b
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-09-27 00:19:28 +0700

    .

--- app/main.py
+++ app/main.py
@@ -24,19 +24,19 @@ app.title("Two Screens")
 app.geometry("800x600")
 app.resizable(False, False)
 
-# Main Screen
+# Màn hình 1
 main_frame = tk.Frame(app, width=800, height=600)
 main_frame.pack()
 
-# Create a Canvas to draw the gradient background
+# Tạo background đỏ của màn hình 1
 canvas = tk.Canvas(main_frame, width=800, height=160, bg="red")
 canvas.pack()
 
-# Create a white background for the bottom half
+# Tạo background trắng của màn hình 1
 bottom_frame = tk.Frame(main_frame, width=800, height=440, bg="white")
 bottom_frame.pack()
 
-# Create an image button instead of text button
+# Image Button
 image = Image.open("face.jpg")  # Replace with your image file path
 image = image.resize((400, 296))  # Resize the image as needed
 photo = ImageTk.PhotoImage(image)
@@ -45,7 +45,7 @@ next_button = tk.Button(main_frame, image=photo, bd=0, command=show_play_screen)
 next_button.photo = photo
 next_button.place(relx=0.5, rely=0.55, anchor=tk.CENTER, width=400, height=290)
 
-# Add an image to the top left corner
+# Logo
 image = Image.open("ptit.png")  # Replace with your image file path
 image = image.resize((100, 100))  # Resize the image as needed
 photo = ImageTk.PhotoImage(image)
@@ -53,12 +53,12 @@ image_label = tk.Label(main_frame, image=photo)
 image_label.photo = photo
 image_label.place(relx=0.05, rely=0.05, anchor=tk.NW)
 
-# Text above the image (centered horizontally)
+# Title App
 font_title = customtkinter.CTkFont(family="Montserrat", size=25, weight= "bold")
 text_label = tk.Label(main_frame, text="FACE RECOGNITION PTIT", font=font_title, fg="white", bg="red")
 text_label.place(relx=0.5, rely=0.1, anchor=tk.N)
 
-# Play Screen (Contains Start, End, Show Buttons)
+# Màn hình thứ 2
 play_frame = tk.Frame(app, bg="red", width=800, height=600)
 
 start_button = ttk.Button(play_frame, text="Start", style="TButton", command=start_clicked)
@@ -71,13 +71,6 @@ style.configure("TButton",
                 font=("Helvetica", 14),
                 background="#44475a",
                 foreground="black")
-
-# Custom style for the Main Screen Next Button
-style.configure("Custom.TButton",
-                font=("Helvetica", 14),
-                padding=10,
-                background="#44475a",  # Background color (Dracula color)
-                foreground="black")     # Text color (black)
 
 start_button.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
 end_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
